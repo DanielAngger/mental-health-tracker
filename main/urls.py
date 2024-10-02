cbb301c29de2f22b27685af567bb06a973b46588
DanielAngger <angger.7d@gmail.com>
2024-10-02 13:55:42 +0700

    Tutorial 5

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
-from main.views import logout_user
+from main.views import add_mood_entry_ajax
+from main.views import logout_user 
 from main.views import login_user
 from main.views import register
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
@@ -20,5 +21,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
-    path('delete/<uuid:id>', delete_mood, name='delete_mood'), # sesuaikan dengan nama fungsi yang dibuat
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'),
+    path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
 ]
